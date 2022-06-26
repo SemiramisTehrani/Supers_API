4281839973dec71811c30b289288cbf63db573f4
unknown <semtehrani@yahoo.com>
2022-06-25 22:06:18 -0700

    modified admin.py , added status error and etc in views.py , get & post work in postman

--- supers/views.py
+++ supers/views.py
@@ -4,9 +4,22 @@ from rest_framework.response import Response
 from .serializers import SuperSerializer
 from .models import Super
 
-@api_view(['GET'])
-def supers_list(request):
-    supers = Super.objects.all()
-    serializer = SuperSerializer(supers, many = True)
 
-    return Response(serializer.data)
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+
+from supers import serializers
+
+@api_view(['GET','POST'])
+def supers_list(request):
+    
+    if request.method == 'GET' :
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many = True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST' :
+         serializer = SuperSerializer(data = request.data)
+         serializer.is_valid(raise_exception = True)
+         serializer.save()
+         return Response(serializer.data, status = status.HTTP_201_CREATED)
